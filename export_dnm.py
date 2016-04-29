a91e1e276191167e5cecac0bddbecfcc44f1025e
Symbian9 <appsoft@ua.fm>
2016-04-29 13:09:24 +0300

    Update export_dnm.py
    fix author string

--- export_dnm.py
+++ export_dnm.py
@@ -9,7 +9,7 @@ from bpy_extras.io_utils import (ImportHelper, ExportHelper, unpack_list, unpack
 bl_info = {
     'name'       : 'YSFS 2.0 - DNM file',
     'description': 'YSFlight scripts | Export all objects in scene to DNM file.',
-    'author'     : 'Symbian9', 'Mr Mofumofu',
+    'author'     : 'Symbian9, Mr Mofumofu',
     'version'    : (2, 0, 1),
     'blender'    : (2, 75, 0),
     'location'   : 'File > Import-Export',
