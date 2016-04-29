41c52ce62f6ee725eb0b8de1062849848b09484c
Symbian9 <appsoft@ua.fm>
2016-04-29 13:07:17 +0300

    Update export_srf.py
    fix author string

--- export_srf.py
+++ export_srf.py
@@ -9,7 +9,7 @@ from bpy_extras.io_utils import (ImportHelper, ExportHelper, unpack_list, unpack
 bl_info = {
     'name'       : 'YSFS 2.0 - SRF file',
     'description': 'YSFlight scripts | Export single selected object to SRF file.',
-    'author'     : 'Symbian9', 'Mr Mofumofu',
+    'author'     : 'Symbian9, Mr Mofumofu',
     'version'    : (2, 0, 1),
     'blender'    : (2, 75, 0),
     'location'   : 'File > Import-Export',
